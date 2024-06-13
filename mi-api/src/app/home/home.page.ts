9f5c1b8dcced174a5bf067ce12e525b311b770e6
marianaacunaduoc <maria.acunas@duocuc.cl>
2024-06-13 16:33:57 -0400

    se crean metodos que hacen la llamada a una api

--- mi-api/src/app/home/home.page.ts
+++ mi-api/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
+  //es un metodo dentro que hace un llamado a taskservice y trae toda la lista de tareas
+  getAllTasks(){
+    this.taskService.getAllTasks()
+    .subscribe(tasks => {
+      console.log(tasks);
+    });
+  }
+
+  //este metodo solo trae una sola tarea
+  getTask(){
+    this.taskService.getTask('2')
+    .subscribe(task => {
+      console.log(task);
+    })
+  }
+
+  //este metodo crea la tarea, no siempre es necesario colocar el id, el sistema por defecto coloca el id
+  createTask(){
+    const task = {
+      //id: '12',
+      userId: '1',
+      title: 'cambiar titulo',
+      completed: true
+    };
+    this.taskService.createTask(task)
+    .subscribe((newTask) => {
+      console.log(newTask);
+    });
+  }
+
+  /*en este metodo colocamos lo que actualizamos en que id lo queremos
+  en el pash solo se coloca lo que se quiere actualiza, no toda la lsita*/
+  updateTask(){
+    const task = {
+      id: '200',
+      userId: '1',
+      title: 'nuevo titulo cambiado',
+      completed: true
+    };
+    this.taskService.updateTask(task)
+    .subscribe(tasks => {
+      console.log(tasks);
+    });
+  }
+  //en este metodo eliminamos datos
+  deleteTask() {
+    this.taskService.deleteTask('1')
+    .subscribe((task) =>{
+      console.log(task);
+    });
+  }
 }
